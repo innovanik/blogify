b36fbfa86366b391442ef405612db275a36fb8ce
innovanik <innovanik.pivotal@gmail.com>
2023-03-20 04:44:41 +0900

    최초 배포

---
+++ blogify-app-api/src/main/java/blogify/app/api/ApiApplication.java
@@ -0,0 +1,21 @@
+package blogify.app.api;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
+public class ApiApplication implements ApplicationRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+	}
+}
